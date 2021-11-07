dc29fabc6551564a7340782feeff8c311fe768a2
Azam Ahmed <aziskool20@gmail.com>
2021-11-06 20:22:01 -0400

    added formatter (prettier) and linter (eslint)

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
+
 const router = express.Router();
 
-/* GET home page. */
 router.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
