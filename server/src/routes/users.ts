17da04814c1f411d56b70acb71fc5615068f708a
Azam Ahmed <aziskool20@gmail.com>
2021-11-08 15:40:30 -0500

    Removed node_modules

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,7 +1,27 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import * as UsersController from '../controllers/UsersController';
 
 const router = express.Router();
 
+/**
+ * @route GET /:id?
+ * @desc Get all users or get a specific user
+ * @access Private
+ */
+router.get('/:id?', UsersController.GET_USERS);
 
+/**
+ * @route PATCH /:id
+ * @desc Edit a specific user
+ * @access Private
+ */
+router.patch('/:id', UsersController.EDIT_USER);
+
+/**
+ * @route DELETE /:id
+ * @desc Delete a specific user
+ * @access Private
+ */
+router.delete('/:id', UsersController.DELETE_USER);
 
 export default router;
