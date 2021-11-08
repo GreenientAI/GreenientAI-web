e793c397cde2f2d5b1ba3c2fa42c1358a8004d99
Azam Ahmed <aziskool20@gmail.com>
2021-11-08 16:12:36 -0500

    Removed .next from git

--- client/pages/stocks/view.tsx
+++ client/pages/stocks/view.tsx
@@ -5,18 +5,18 @@ const Chart = dynamic(() => import('../../components/Chart'), {
   ssr: false,
 });
 
-const View: React.FC = ({ }) => {
-  const [data, setData] = useState([]);
+const View: React.FC = ({}) => {
+  const [data, setData] = useState(null);
   useEffect(() => {
     // Get historical data from yahoo finance api using axios
-    axios.get('https://query1.finance.yahoo.com/v8/finance/chart/AAPL?region=US&lang=en-US&interval=1wk&useYfid=true&range=max&.tsrc=finance')
-      .then(res => {
-        console.log(res.data)
-        setData(res.data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
+    // axios.get('https://query1.finance.yahoo.com/v8/finance/chart/AAPL?region=US&lang=en-US&interval=1wk&useYfid=true&range=max&.tsrc=finance')
+    //   .then(res => {
+    //     console.log(res.data)
+    //     setData(res.data);
+    //   })
+    //   .catch(err => {
+    //     console.log(err);
+    //   });
   }, []);
 
   return (
